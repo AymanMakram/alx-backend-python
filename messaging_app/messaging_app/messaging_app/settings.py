4b0c309ba2c2cdda0b36168fcff38130e5acb5e1
Ayman Makram <aymanmmokhtar@gmail.com>
2025-11-15 20:11:15 +0200

    Project setup messaging_app

--- messaging_app/messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/messaging_app/settings.py
@@ -44,16 +44,15 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
     ],
 }
 
-["rest_framework.permissions.IsAuthenticated", "DEFAULT_AUTHENTICATION_CLASSES", "rest_framework.authentication.SessionAuthentication"]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
