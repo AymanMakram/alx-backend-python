10aecddccaa312878fb39340dcc6231cca21d0e2
Ayman Makram <aymanmmokhtar@gmail.com>
2025-11-15 20:13:49 +0200

    Project setup messaging_app

--- messaging_app/messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/messaging_app/settings.py
@@ -43,15 +43,7 @@ INSTALLED_APPS = [
     'chats',
 ]
 
-REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
-    ],
-}
-
+["rest_framework.permissions.IsAuthenticated", "DEFAULT_AUTHENTICATION_CLASSES", "rest_framework.authentication.SessionAuthentication"]
 
 
 MIDDLEWARE = [
