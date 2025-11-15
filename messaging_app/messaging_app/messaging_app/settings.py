e5f8532652c00caa56f80763a099e6304ddb7215
Ayman Makram <aymanmmokhtar@gmail.com>
2025-11-15 19:52:45 +0200

    Update Project setup messaging_app

--- messaging_app/messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/messaging_app/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
