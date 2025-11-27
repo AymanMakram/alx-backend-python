15b38d2e3c35793c50959ddfec3f8584976b36fc
Ayman Makram <aymanmmokhtar@gmail.com>
2025-11-27 16:35:27 +0200

    Update Use Signals for Deleting User-Related Data

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,8 @@
+from django.db.models.signals import post_delete
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import Message, MessageHistory
+from django.contrib.auth.models import User
+from .models import Message, MessageHistory, Notification
 
 @receiver(pre_save, sender=Message)
 def save_old_message_content(sender, instance, **kwargs):
@@ -23,3 +25,24 @@ def save_old_message_content(sender, instance, **kwargs):
             old_content=old_msg.content
         )
         instance.edited = True
+
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    """
+    Delete all messages, notifications and message histories
+    when a user is deleted.
+    """
+
+    # Delete messages SENT by the user
+    Message.objects.filter(sender=instance).delete()
+
+    # Delete messages RECEIVED by the user
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete message history linked to those messages
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    MessageHistory.objects.filter(message__receiver=instance).delete()
+
+    # Delete user's notifications
+    Notification.objects.filter(user=instance).delete()
